62e4a1e4da2b7cc5fa8fa4a56793b254b2638512
Umeshkumarkatravath <umeshkumarkatravath86@gmail.com>
2024-11-16 20:20:59 +0530

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,9 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        int[] numbers = {5, 10, 15, 20, 25};
+        System.out.println("Array elements:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
